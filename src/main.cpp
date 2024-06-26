76a2128c5f531294a5dd380337c0625f6f666f8f
SeaSparrowOG <91731172+SeaSparrowOG@users.noreply.github.com>
2024-06-26 09:40:32 +0300

    Project initialization

---
+++ src/main.cpp
@@ -0,0 +1,70 @@
+#include <spdlog/sinks/basic_file_sink.h>
+
+namespace Log {
+    void SetupLog() {
+        auto logsFolder = SKSE::log::log_directory();
+        if (!logsFolder) SKSE::stl::report_and_fail("SKSE log_directory not provided, logs disabled.");
+
+        auto pluginName = Version::PROJECT;
+        auto logFilePath = *logsFolder / std::format("{}.log", pluginName);
+        auto fileLoggerPtr = std::make_shared<spdlog::sinks::basic_file_sink_mt>(logFilePath.string(), true);
+        auto loggerPtr = std::make_shared<spdlog::logger>("log", std::move(fileLoggerPtr));
+
+        spdlog::set_default_logger(std::move(loggerPtr));
+        spdlog::set_level(spdlog::level::info);
+        spdlog::flush_on(spdlog::level::info);
+
+        //Pattern
+        spdlog::set_pattern("%v");
+    }
+}
+void MessageHandler(SKSE::MessagingInterface::Message* a_message) {
+    switch (a_message->type) {
+    case SKSE::MessagingInterface::kDataLoaded:
+        break;
+    case SKSE::MessagingInterface::kNewGame:
+    case SKSE::MessagingInterface::kPostLoadGame:
+        break;
+    default:
+        break;
+    }
+}
+
+#ifdef SKYRIM_AE
+extern "C" DLLEXPORT constinit auto SKSEPlugin_Version = []() {
+    SKSE::PluginVersionData v;
+    v.PluginVersion({ Version::MAJOR, Version::MINOR, Version::PATCH });
+    v.PluginName(Version::PROJECT);
+    v.AuthorName(Version::AUTHOR);
+    v.UsesAddressLibrary();
+    v.UsesUpdatedStructs();
+    v.CompatibleVersions({
+        SKSE::RUNTIME_1_6_1130,
+        _1_6_1170 });
+    return v;
+    }();
+#else 
+SKSEPluginInfo(
+    .Name = Version::PROJECT
+)
+#endif
+
+extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface * a_skse) {
+    Log::SetupLog();
+    _loggerInfo("Starting up {}.", Version::PROJECT);
+    _loggerInfo("Plugin Version: {}.{}.{}", Version::MAJOR, Version::MINOR, Version::PATCH);
+    _loggerInfo("Version build:");
+
+#ifdef SKYRIM_AE
+    _loggerInfo("    >Latest Version. Maintained by: {}.", Version::AUTHOR);
+#else
+    _loggerInfo("    >1.5 Version. Do not report ANY issues with this version.");
+#endif
+    _loggerInfo("----------------------------------------------------------------");
+
+    SKSE::Init(a_skse);
+    auto messaging = SKSE::GetMessagingInterface();
+    messaging->RegisterListener(MessageHandler);
+
+    return true;
+}
